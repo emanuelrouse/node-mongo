d4b7fefdb71fba9fdaf353e37555e7732eb0185f
Emanuel Rouse <41143874+emanuelrouse@users.noreply.github.com>
2022-01-29 10:58:59 -0500

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient; // Client for the mongo server
-
-const test = require('assert');
+const assert = require('assert');
+const dboper = require('./operations');
 
 // Connection url
 
@@ -20,30 +20,84 @@ MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
     // create the campsites collection 
     // db.createCollection("campsites");
     // Still created some crazy bugs
-    // Main takeaway with the tests is to make sure your collection is created before dropping the collection
+    // Main takeaway with the asserts is to make sure your collection is created before dropping the collection
     db.dropCollection('campsites', function (err, result) {
 
-        const collection = db.collection('campsites');
 
-        test.strictEqual(err, null);
+        assert.strictEqual(err, null);
 
         console.log('Droppped Collection', result);
 
-        collection.insertOne({ name: "Breadcrumb Trail Campground", description: "Test" },
-            function (err, result) {
-                test.strictEqual(err, null);
-                console.log('Insert Document:', result.ops);
+        dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "test" }, 'campsites', result => {
+            console.log('Insert Document:', result.ops);
 
-                collection.find().toArray(function (err, docs) {
-                    test.strictEqual(err, null);
-                    console.log('Found Documents', docs);
+            dboper.findDocuments(db, 'campsites', docs => {
+                console.log('Found Documents', docs);
 
-                    client.close();
+                dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" }, { description: "Updated Test Description" }, 'campsites', result => {
+                    console.log('Updated Document Count:', result.result.nModified);
+
+                    dboper.findDocuments(db, 'campsites', docs => {
+                        console.log('Found Documents', docs);
+
+                        dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+                            'campsites', result => {
+                                console.log('Deleted Document Count:', result.deletedCount);
+
+                                client.close();
+                            }
+                        );
+                    })
                 });
             });
+        });
     });
-
 });
 
+
+// MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+
+//     assert.strictEqual(err, null);
+
+//     console.log('Connected correctly to server');
+
+//     const db = client.db(dbname);
+
+//     db.dropCollection('campsites', (err, result) => {
+//         assert.strictEqual(err, null);
+//         console.log('Dropped Collection:', result);
+
+//         dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test" },
+//             'campsites', result => {
+//                 console.log('Insert Document:', result.ops);
+
+//                 dboper.findDocuments(db, 'campsites', docs => {
+//                     console.log('Found Documents:', docs);
+
+//                     dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
+//                         { description: "Updated Test Description" }, 'campsites',
+//                         result => {
+//                             console.log('Updated Document Count:', result.result.nModified);
+
+//                             dboper.findDocuments(db, 'campsites', docs => {
+//                                 console.log('Found Documents:', docs);
+
+//                                 dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+//                                     'campsites', result => {
+//                                         console.log('Deleted Document Count:', result.deletedCount);
+
+//                                         client.close();
+//                                     }
+//                                 );
+//                             });
+//                         }
+//                     );
+//                 });
+//             });
+//     });
+// });
 // err callback pattern is used because we're working with asynchronus operations
-// communication between node app and mongodb server is not synchronus 
+// communication between node app and mongodb server is not synchronus
+// Defining vs Calling
+// Defining: Will do once run
+// Calling: Execute the code
